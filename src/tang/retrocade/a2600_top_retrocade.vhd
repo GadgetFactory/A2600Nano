ba41e2120b6b8d8589d6a3f7708568b26714f389
Jack Gassett <jack.gassett@gadgetfactory.net>
2026-05-05 09:42:27 -0600

    Comment out unused PMOD pins to eliminate synthesis warnings
    A2600 core doesn't use PMOD connector - commented out in both VHDL
    entity and CST file to prevent 'unused inout' warnings.
    
    Pins are documented and can be uncommented for other cores that
    need PMOD GPIO functionality.

--- src/tang/retrocade/a2600_top_retrocade.vhd
+++ src/tang/retrocade/a2600_top_retrocade.vhd
@@ -42,9 +42,9 @@ entity A2600_top is
     sd_clk      : out std_logic;
     sd_cmd      : inout std_logic;
     sd_dat      : inout std_logic_vector(3 downto 0);
-    -- pmod interface
-    pmod_ioa    : inout std_logic_vector(3 downto 0);
-    pmod_iob    : inout std_logic_vector(3 downto 0);
+    -- pmod interface (commented out - not used by A2600 core)
+    -- pmod_ioa    : inout std_logic_vector(3 downto 0);
+    -- pmod_iob    : inout std_logic_vector(3 downto 0);
     -- ws2812 rgb led
     ws2812      : out std_logic
     );
